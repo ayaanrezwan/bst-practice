993772b31715be3ba99d8030dcb37013c98d27e9
ayaanrezwan <arezwan9460@outlook.com>
2026-04-05 23:08:01 -0400

    Testing vscode setup

--- main.cpp
+++ main.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main() {
-    cout << 5 + 3 << endl;
+    int a = 3;
+    int b = 4;
+
+    if (a > b) {
+        cout << a - b << endl;
+    } else {
+        cout << b - a << endl;
+    }
+
     return 0;
 }
